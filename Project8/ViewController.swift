6fd3787f360e5fad20c8cf604ca8154273e03ed6
tborysova <borysova.tetiana@gmail.com>
2022-10-18 14:43:14 +0200

    Initial Commit

---
+++ Project8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Tetiana Borysova on 18.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
